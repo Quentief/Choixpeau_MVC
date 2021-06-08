ed60dd6a29999db8062cf2bf46e7c855c9b6a440
Quentin Tiefaine <quentief@gmail.com>
2021-06-08 12:24:02 -0400

    requirements added

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@
 # pip install pandas
 # pip install openpyxl
 
+# pip install -r requirements.txt
+
 from Controller import Start
 
 if __name__ == '__main__':      # permet de lancer le programme en executant le code
